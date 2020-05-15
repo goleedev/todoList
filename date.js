8c2488c9be83c7656de1bf2e012c6f99fd56f8ad
GO Lee <golee.dev@gmail.com>
2020-05-15 21:42:06 +0900

    todo list using esj

---
+++ date.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+exports.getDate = function () {
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+    const today = new Date();
+    return today.toLocaleDateString("en-US", options);
+};
+
+exports.getDay = function () {
+    const options = {
+        weekday: 'long',
+    };
+    const today = new Date();
+    return today.toLocaleDateString("en-US", options);
+};
